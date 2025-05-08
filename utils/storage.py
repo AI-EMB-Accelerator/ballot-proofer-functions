503299dbf3eaa49b1633bb28f692d8dce7662187
IsabellaS09 <isabellamseeman@gmail.com>
2025-05-08 08:04:14 -0500

    Adding function to upload a document into azure storage (#1)
    Co-authored-by: Ella S <ellas@Ellas-MBP.attlocal.net>

--- utils/storage.py
+++ utils/storage.py
@@ -1,8 +1,31 @@
 """Module for managing storage operations."""
 
+import os
+import uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, ContentSettings
+
 def save_to_blob_storage(file_path, blob_name):
     """
     Save a file to Azure Blob Storage.
     """
+    account_url = os.getenv("AZURE_DOCUMENT_STORAGE_ENDPOINT") or ""
+    credential = os.getenv("AZURE_DOCUMENT_STORAGE_KEY") or ""
+    container_name = os.getenv("AZURE_DOCUMENT_STORAGE_CONTAINER_NAME") or ""
 
-    return "BLOB URL"
+    if account_url is "" or credential is "" or container_name is "":
+        raise ValueError(
+            "Please set the Azure Document environment variables"
+        )
+
+    blob_service_client = BlobServiceClient(account_url, credential=credential)
+
+    blob_name=f"{uuid.uuid4()}.pdf"
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    
+    with open(file_path, 'rb') as file:
+        blob_client.upload_blob(data=file, overwrite=True, content_settings=ContentSettings(content_type='application/pdf'))
+    print(f"Document {blob_name} uploaded successfully")
+
+    return f"{account_url}/{container_name}/{blob_name}"
