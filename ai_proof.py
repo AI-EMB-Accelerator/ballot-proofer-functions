54ab4e2b20ae29be6399e2d65aa206de192455d8
Keith Fung <keith.fung@infernored.com>
2025-05-09 14:10:12 -0400

    Cleanup

--- ai_proof.py
+++ ai_proof.py
@@ -13,7 +13,6 @@ from utils.storage import save_data_to_blob_storage
 load_dotenv()
 
 endpoint = os.getenv("AZURE_OPEN_AI_ENDPOINT", "")
-# model_name = "gpt-4.1-mini"
 deployment = os.getenv("AZURE_OPEN_AI_DEPLOYMENT", "")
 
 subscription_key = os.getenv("AZURE_OPEN_AI_KEY", "")
@@ -23,6 +22,18 @@ client = AzureOpenAI(
     api_version=api_version,
     azure_endpoint=endpoint,
     api_key=subscription_key,
+)
+
+reasoning_endpoint = os.getenv("O3_AZURE_OPEN_AI_ENDPOINT", "")
+reasoning_deployment = os.getenv("O3_AZURE_OPEN_AI_DEPLOYMENT", "")
+
+reasoning_subscription_key = os.getenv("O3_AZURE_OPEN_AI_KEY", "")
+reasoning_api_version = os.getenv("O3_AZURE_OPEN_AI_API_VERSION", "")
+
+reasoning_client = AzureOpenAI(
+    api_version=reasoning_api_version,
+    azure_endpoint=reasoning_endpoint,
+    api_key=reasoning_subscription_key,
 )
 
 OUTPUT_FILE = "ballot_proof.json"
@@ -60,11 +71,11 @@ response = client.chat.completions.create(
 
 test_ballot_definition = response.choices[0].message.content
 
-with open('./output/definitions/ballot_definition.json', 'r') as file: 
+with open('./input/definitions/ballot_definition.json', 'r') as file: 
     reference_ballot_definition = json.load(file) # FIX THIS
 
 # Step 3 - Proof
-response = client.chat.completions.create(
+response = reasoning_client.chat.completions.create(
     messages=[
         {
             "role": "system",
@@ -76,12 +87,12 @@ response = client.chat.completions.create(
         },
     ],
     response_format={"type": "json_object"},
-    max_completion_tokens=32000,
+    max_completion_tokens=20000,
     temperature=1.0,
     top_p=1.0,
     frequency_penalty=0.0,
     presence_penalty=0.0,
-    model=deployment,
+    model=reasoning_deployment,
 )
 
 # Step 4 - Save the proofing result
